9221b6bb99e2f50e589fc829dccf9282e9a93d53
HossyWorlds <hta43435@gmail.com>
2025-06-10 06:44:31 +0900

    add: sandbox.go

---
+++ go_tour_exercises/sandbox.go
@@ -0,0 +1,14 @@
+// ✅ https://go-tour-jp.appspot.com/welcome/4
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Welcome to the playground!")
+
+	fmt.Println("The time is", time.Now())
+}
